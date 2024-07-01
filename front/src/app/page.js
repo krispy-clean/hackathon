1e13ea6fb5a2194e52e3dcc2c965d0e509c1ca21
Barnabé PILLIAUDIN <barnabe.pilliaudin@gmail.com>
2024-07-01 14:07:39 +0200

    chore: remove pages folder from content

--- front/src/app/page.js
+++ front/src/app/page.js
@@ -1,11 +1,11 @@
-import { Card, CardBody, CardHeader } from "@nextui-org/react"
+import {Card, CardBody, CardHeader} from "@nextui-org/react"
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <Card>
         <CardHeader>
-          <h2>Hackathon has started</h2>
+          <h2 className="text-yellow-500">Hackathon has started</h2>
         </CardHeader>
         <CardBody>
           <p>Good luck everyone</p>
