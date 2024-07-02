b68919a0f0c4ec9077679ffdabc010f3f571a876
Meyer <meyer@attal.dev>
2024-07-02 13:40:43 +0200

    fix yup import and config

--- apps/back/src/utils/config.ts
+++ apps/back/src/utils/config.ts
@@ -1,27 +1,26 @@
-import * as yup from "yup";
-import {ValidationError} from "yup";
-import {requiredNumber, requiredString} from "./schemas";
+import yup, { ValidationError } from "yup";
+import { requiredNumber, requiredString } from "./schemas";
 
-const configSchema =yup.object({
-    port: requiredNumber,
-    hostname: requiredString,
-    protocol: requiredString,
-})
+const configSchema = yup.object({
+  port: requiredNumber,
+  hostname: requiredString,
+  protocol: requiredString,
+});
 
 export type Config = yup.InferType<typeof configSchema>;
-let config = {}
+let config = {};
 
 try {
-    config = configSchema.validate({
-        port:process.env.BACKEND_PORT,
-        hostname:process.env.BACKEND_HOSTNAME,
-        protocol:process.env.BACKEND_PROTOCOL,
-    })
-}catch (error) {
-    if (error instanceof ValidationError) {
-        throw new InvalidConfigError(error.errors);
-    }
-    throw error;
+  config = await configSchema.validate({
+    port: process.env.BACKEND_PORT,
+    hostname: process.env.BACKEND_HOSTNAME,
+    protocol: process.env.BACKEND_PROTOCOL,
+  });
+} catch (error) {
+  if (error instanceof ValidationError) {
+    throw new InvalidConfigError(error.errors);
+  }
+  throw error;
 }
 
 export default config as Config;
